42ceb500800c22a7506700ddf876bde5584ebf10
Oscar Shoudt <shoudto@gmail.com>
2019-05-02 01:17:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  
+  count = 0 
+  collection = []
+  
+  while count < array.count 
+    collection << yield(array[i])
+    count += 1 
+  end 
+  collection 
+end 
 
-
+my_collect(['ruby', 'javascript', 'python', 'objective-c'])
